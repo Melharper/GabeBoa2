bea84df0ba11f5b6d43555b457e37c5b07fc8520
Melharper <melharper728@gmail.com>
2024-11-30 02:22:04 -0600

    Update url_decoder.lua

--- url_decoder.lua
+++ url_decoder.lua
@@ -1,7 +1,7 @@
 local HttpService = game:GetService("HttpService")
 
--- Base64-encoded whitelist (with additional UserId)
-local encodedWhitelist = "NzcwMTIxODA=,MjM4MDYzNDcyNw==,NjkyODU4NDA="  -- Base64 encoded version of {77012180, 2380634727, 69285840}
+-- Base64-encoded whitelist and URL (obfuscated)
+local encodedWhitelist = "NzcwMTIxODA=,MjM4MDYzNDcyNw=="  -- Base64 encoded version of {77012180, 2380634727}
 local encodedUrl = "aHR0cHM6Ly9yYXcuZ2l0aHViLmNvbS9NZWxoYXJwZXIvR2FiZUJvYTIvcmVmcy9oZWFkcy9tYWluL0h1YiUyMEF1dG8lMjBmYXJtLmx1YQ=="
 
 -- Decode Base64 function
@@ -28,6 +28,10 @@ end
 -- Decode whitelist and URL
 local decodedWhitelist = decodeBase64(encodedWhitelist)
 local decodedUrl = decodeBase64(encodedUrl)
+
+-- Print out the decoded values for debugging
+print("Decoded Whitelist:", decodedWhitelist)
+print("Decoded URL:", decodedUrl)
 
 -- Function to check if the player is whitelisted
 local function isWhitelisted(userId)
